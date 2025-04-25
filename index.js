fa54245b6459c7b51df9ced958d150a011b83340
Prashant Kumar <kumarprashant8986@gmail.com>
2025-04-25 23:18:39 +0530

    set trust proxy

--- index.js
+++ index.js
@@ -21,6 +21,7 @@ app.use(cors({
   origin: process.env.CLIENT_URL || 'http://localhost:5173',
   credentials: true
 }));
+app.set('trust proxy', true);
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
